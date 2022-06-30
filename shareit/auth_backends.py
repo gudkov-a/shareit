b9b76499036b426103445e05ef2607433a2398f6
Alexey Gudkov <gudkov.alexey.s@gmail.com>
2022-06-30 18:16:27 +0300

    Fixed custom auth class

--- shareit/auth_backends.py
+++ shareit/auth_backends.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 
+from typing import Optional
+
 from django.contrib.auth.models import User
-from api.models import AuthLog
 from django.conf import settings
+from django.contrib.auth.backends import BaseBackend
+
+from api.models import AuthLog
 
 
-class AntiBruteBackend:
+class AntiBruteBackend(BaseBackend):
     """
     Prevent site from brute force attacks
     """
 
     def authenticate(self, request, username=None, password=None):
-        remote_addr = request.META.get('REMOTE_ADDR')
-        num_of_attempts = AuthLog.get_attempts_by_ip_address(remote_addr)
-        if num_of_attempts > settings.ATTEMPTS_TO_LOGIN - 1:
+        remote_addr = self._extract_remote_addr(request)
+        if AuthLog.is_host_reached_auth_attempts_limit(remote_addr):
             if settings.DEBUG:
                 print('Too many login failures!')
             return None
@@ -25,6 +28,13 @@ class AntiBruteBackend:
         # Log failure
         AuthLog.objects.create(remote_address=remote_addr)
         return None
+
+    def _extract_remote_addr(self, req: Optional) -> str:
+        if req is not None and hasattr(req, 'META'):
+            remote_addr = req.META.get('REMOTE_ADDR')
+        else:
+            remote_addr = '127.0.0.1'
+        return remote_addr
 
     def get_user_by_username(self, username):
         try:
